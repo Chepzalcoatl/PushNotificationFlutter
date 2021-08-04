06978341843bcefe5be3d98922dd66c9d8156d12
Eduardo Barron <st.buendia@gmail.com>
2021-08-04 08:12:13 -0500

    feature: send messages to realtime database

--- lib/screens/messages_screen.dart
+++ lib/screens/messages_screen.dart
@@ -1,20 +1,28 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:login/data/message_dao.dart';
+import 'package:login/models/message.dart';
 import 'package:login/res/custom_colors.dart';
 import 'package:login/screens/user_info_screen.dart';
 import 'package:login/widgets/app_bar_title.dart';
 
 class MessagesScreen extends StatefulWidget {
-  const MessagesScreen({Key? key, required this.user}) : super(key: key);
+  MessagesScreen({Key? key, required this.user}) : super(key: key);
   final User user;
+  final messageDao = MessageDao();
 
   @override
   MessagesScreenPageState createState() => MessagesScreenPageState();
 }
 
 class MessagesScreenPageState extends State<MessagesScreen> {
+  TextEditingController _messageController = TextEditingController();
+  ScrollController _scrollController = ScrollController();
   late User _user;
-  
+
   @override
   void initState() {
     _user = widget.user;
@@ -23,18 +31,53 @@ class MessagesScreenPageState extends State<MessagesScreen> {
 
   @override
   Widget build(BuildContext context) {
+    WidgetsBinding.instance!.addPostFrameCallback((_) => _scrollToBottom());
     return Scaffold(
       appBar: AppBar(
         elevation: 10,
         backgroundColor: CustomColors.firebaseNavy,
-        title: AppHomeBar(),
+        title: _appHomeBar(),
       ),
-      body: Center(child: Text('Hola '+ _user.displayName!)),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Flexible(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                    child: TextField(
+                      keyboardType: TextInputType.text,
+                      controller: _messageController,
+                      onChanged: (text) => setState(() {}),
+                      onSubmitted: (input) {
+                        _sendMessage();
+                      },
+                      decoration:
+                          const InputDecoration(hintText: 'Enter new message'),
+                    ),
+                  ),
+                ),
+                IconButton(
+                    icon: Icon(_canSendMessage()
+                        ? CupertinoIcons.arrow_right_circle_fill
+                        : CupertinoIcons.arrow_right_circle),
+                    onPressed: () {
+                      _sendMessage();
+                    })
+              ],
+            ),
+            _getMessageList(),
+          ],
+        ),
+      ),
     );
   }
 
-  AppHomeBar() {
-   return Row(
+  _appHomeBar() {
+    return Row(
       crossAxisAlignment: CrossAxisAlignment.center,
       mainAxisSize: MainAxisSize.min,
       children: [
@@ -73,5 +116,78 @@ class MessagesScreenPageState extends State<MessagesScreen> {
         ),
       ],
     );
- }
+  }
+
+  bool _canSendMessage() => _messageController.text.length > 0;
+
+  void _sendMessage() {
+    if (_canSendMessage()) {
+      final message = Message(_messageController.text, DateTime.now());
+      widget.messageDao.saveMessage(message);
+      _messageController.clear();
+      setState(() {});
+    }
+  }
+
+  Widget _getMessageList() {
+    return Expanded(
+      child: FirebaseAnimatedList(
+        controller: _scrollController,
+        query: widget.messageDao.getMessageQuery(),
+        itemBuilder: (context, snapshot, animation, index) {
+          final json = snapshot.value as Map<dynamic, dynamic>;
+          final message = Message.fromJson(json);
+          return _messageWidget(message.text, message.date);
+        },
+      ),
+    );
+  }
+
+  void _scrollToBottom() {
+    if (_scrollController.hasClients) {
+      _scrollController.jumpTo(_scrollController.position.maxScrollExtent);
+    }
+  }
+
+  Widget _messageWidget(String message, DateTime date) {
+    return Padding(
+        padding: const EdgeInsets.only(left: 1, top: 5, right: 1, bottom: 2),
+        child: Column(
+          children: [
+            Container(
+                decoration: BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.grey[350]!,
+                          blurRadius: 2.0,
+                          offset: Offset(0, 1.0))
+                    ],
+                    borderRadius: BorderRadius.circular(50.0),
+                    color: Colors.white),
+                child: MaterialButton(
+                    disabledTextColor: Colors.black87,
+                    padding: EdgeInsets.only(left: 18),
+                    onPressed: null,
+                    child: Wrap(
+                      children: <Widget>[
+                        Container(
+                            child: Row(
+                          children: [
+                            Text(message),
+                          ],
+                        )),
+                      ],
+                    ))),
+            Padding(
+              padding: const EdgeInsets.only(top: 4),
+              child: Align(
+                  alignment: Alignment.topRight,
+                  child: Text(
+                    DateFormat('yyyy-MM-dd, kk:mma').format(date).toString(),
+                    style: TextStyle(color: Colors.grey),
+                  )),
+            ),
+          ],
+        ));
+  }
 }
